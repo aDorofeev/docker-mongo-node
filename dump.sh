289b4a871cdf467ae701bfdbdf99ae8c69e0bc7b
klamas1 <klamas1@gmail.com>
2019-08-08 22:31:50 +0500

    Delete dump.sh

--- dump.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-set -o nounset
-
-docker-compose exec -T mongodb bash -c 'cd /data/db/dump; rm -f directheroes.tar.gz; mongodump --out=/data/db/dump --db directheroes; tar pczf directheroes.tar.gz directheroes'
-
